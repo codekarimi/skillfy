75284c8e66288e02d3db62da970e39e1ff7ea239
codekarimi <ckarimi676@gmail.com>
2025-09-29 21:59:00 +0300

    Resume Page

--- app/routes/resume.tsx
+++ app/routes/resume.tsx
@@ -1,13 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React, { useEffect, useState } from 'react'
+import { Link, useNavigate, useParams } from 'react-router'
 import ATS from '~/components/ATS';
 import Details from '~/components/Details';
 import Summary from '~/components/Summary';
-import { resumes } from '~/constants'
+import { usePuterStore } from '~/lib/puter';
 
 const resume = () => {
 
-    const Resume = resumes;
+    const { auth, isLoading, fs, kv } = usePuterStore();
+    const { id } = useParams();
+    const [imageUrl, setImageUrl] = useState('');
+    const [resumeUrl, setResumeUrl] = useState('');
+    const [feedback, setFeedback] = useState<Feedback | null>(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      
+        const loadResume = async () => {
+        
+            if (!id) return;
+            
+            
+            const resumeData = await kv.get(`resume:${id}`);
+    
+            if(!resumeData) return;
+    
+            const data = JSON.parse(resumeData);
+    
+            const resumeBlob = await fs.read(data.resumePath);
+                if(!resumeBlob) return;
+    
+                // URL.createObjectURL(imageBlob) is a web API method that creates a temporary URL that can be used to reference a file or blob directly in the browser without needing to upload it to a server.
+    
+            const pdfBlob = new Blob([resumeBlob], { type: 'application/pdf' });
+                const resumeUrl = URL.createObjectURL(pdfBlob);
+                setResumeUrl(resumeUrl);
+    
+            const imageBlob = await fs.read(data.imagePath);
+                if(!imageBlob) return;
+                const imageUrl = URL.createObjectURL(imageBlob);
+                setImageUrl(imageUrl);
+    
+    
+            setFeedback(data.feedback);
+    
+    
+                console.log({resumeUrl, imageUrl, feedback: data.feedback });
+            }
+    
+            loadResume();
+        },[id]);
+            
   return (
     <main className="!pt-0">
 
@@ -16,17 +59,20 @@ const resume = () => {
                     <img src="/icons/back.svg" alt="logo" className="w-2.5 h-2.5" />
                     <span className="text-gray-800 text-sm font-semibold">Back to Homepage</span>
                 </Link>
+                <Link to="/wipe">
+                <button className="primary-button w-fit text-xl font-semibold"><p>Wipe Data</p></button>
+                </Link>  
             </nav>
-            {resumes.map((resume) => (
+
+            
             <div className="flex flex-row w-full max-lg:flex-col-reverse">
             
                 <section className="feedback-section bg-[url('/images/bg-small.svg') bg-cover h-[100vh] sticky top-0 items-center justify-center">
+                {imageUrl && resumeUrl && (
                  <div className="animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit">
-
-                 
-                            <a href={resume.resumePath} target="_blank" rel="noopener noreferrer">
+                            <a href={resumeUrl} target="_blank" rel="noopener noreferrer">
                                 <img
-                                    src={resume.imagePath}
+                                    src={imageUrl}
                                     className="w-full h-full object-contain rounded-2xl"
                                     title="resume"
                                 />
@@ -34,19 +80,25 @@ const resume = () => {
                 
                             
                         </div>
+                )}
                 </section>
+                
                 <section className="feedback-section">
                     <h2 className="text-4xl !text-black font-bold">Resume Review</h2>
+                    {feedback ? (
                     <div className="flex flex-col gap-8 animate-in fade-in duration-1000">
-                            <Summary feedback={resume.feedback} />
-                            {/* <ATS score={resume.feedback.ATS.score || 0} suggestions={resume.feedback.ATS.tips || []} />
-                            <Details feedback={resume.feedback} /> */}
+                            <Summary feedback={feedback} />
+                            {/* <ATS score={resume.feedback.ATS.score || 0} suggestions={resume.feedback.ATS.tips || []} /> */}
+                            <Details feedback={feedback} />  
                         </div>
+                    ):(
+                        <img src="/images/resume-scan-2.gif" className="w-full" />
+                    )}
                     
                 </section>
                 
             </div>
-            ))}
+    
     </main>
   )
 }
