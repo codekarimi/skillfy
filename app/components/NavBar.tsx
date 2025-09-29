95c1e55b4f6aa287df5be64a486925765e3a93e5
codekarimi <ckarimi676@gmail.com>
2025-09-29 21:53:47 +0300

    logout Nav

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,14 +1,18 @@
 import {Link} from "react-router";
+import { usePuterStore } from "~/lib/puter";
+
 
 const Navbar = () => {
+    
+    const { isLoading, auth } = usePuterStore();
     return (
         <nav className="navbar">
             <Link to="/">
                 <p className="text-2xl font-bold text-gradient">SKILLFY🚀</p>
             </Link>
-            <Link to="/upload" className="primary-button w-fit">
-                Upload Resume
-            </Link>
+           
+            <button className="primary-button w-fit text-xl font-semibold" onClick={auth.signOut}><p>Log out</p></button>
+            
         </nav>
     )
 }
